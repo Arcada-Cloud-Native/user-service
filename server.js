48ece8b5ea8734a4f00dec073ce33076ff0b82a5
nordluma <nordluma@arcada.fi>
2021-02-08 17:28:52 +0200

    api documentation and added db connection

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-require("dotenv").config();
+require("dotenv").config({ path: "./config/.env" });
 
-const app = express();
+const connectDb = require("./config/db");
+const userRoutes = require("./routes/users");
 
 // Connect DB
 
+const app = express();
+
 // Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Routes
+app.use("/api/users", userRoutes);
 
 // Error handling
 
 const PORT = process.env.PORT || 8000;
 
-app.listen((PORT) => {
+app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
 });
