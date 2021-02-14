c1ce207fc29f0df8975eff550439e1211da9d93a
Skeba <seba.lundgren@gmail.com>
2021-02-14 19:15:55 +0200

    users.model.js
    Skapar /signup för post request så man kan registrera en användare med email och lösen.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,58 @@
 const route = require("express").Router();
+const express = require('express');
+const mongoose = require('mongoose');
+//const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/users.model');
+
 
 // Routes(GET, POST, PUT, DELETE)
+route.get('/', (req, res, next) => {
+    User.find().exec()
+        .then(documents => {
+            res.status(200).json(documents);
+        })
+        .catch(error => {
+            console.log(error);
+            const err = new Error(error);
+            err.status = error.status || 500;
+            
+            next(err);
+        });
+});
+
+route.post('/signup', (req, res, next) => {
+    
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+            res.status(500).json({
+                message: err 
+            });
+        }  
+        else {
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash 
+            });
+        
+            user.save()
+                .then(result => {
+                    res.status(201).json({
+                        message: "User registered!"
+                    });
+                })
+                .catch(error => {
+                    console.log(error);
+                    const err = new Error(error);
+                    err.status = error.status || 500;
+                    
+                    next(err);
+                });
+        }    
+    });
+    
+});
 
 module.exports = route;
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/api/users", userRoutes);
 
 // Error handling
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
