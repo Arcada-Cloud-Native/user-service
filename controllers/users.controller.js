2526843b6d1fa7db0bf322d226f42f45021f2f54
nordluma <nordluma@arcada.fi>
2021-02-15 15:40:34 +0200

    fixed a typo

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,7 +5,7 @@ const User = require("../models/users.model");
 // @desc    Get All Users
 // @route   GET /api/users
 // @access  Public
-exports.getAllusers = async (req, res, next) => {
+exports.getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
         res.status(200).json(users);
