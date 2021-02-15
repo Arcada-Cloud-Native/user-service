68194da484546ee19b56cee54bd5e208db5fe529
nordluma <nordluma@arcada.fi>
2021-02-15 11:57:31 +0200

    removed unecessary imports and started controllers

---
+++ controllers/users.controller.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// @desc    Get All Users
+// @route   GET /api/users
+// @access  Public
+
+// @desc    Register user
+// @route   POST /api/users/signup
+// @access  Public
+
+// @desc    User Login
+// @route   POST /api/users
+// @access  Public
+
+// @desc    User Logout
+// @route   GET /api/users/logout
+// @access  Secure
