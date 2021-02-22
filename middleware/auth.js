d3cd4d1d8a66e9247633178f305068dee907ec0f
nordluma <nordluma@arcada.fi>
2021-02-22 17:54:58 +0200

    check authorization

---
+++ middleware/auth.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+exports.checkAuth = (req, res, next) => {
+    const token = req.header("Authorization");
+    if (!token) res.status(401).json({ message: "Auth failed" });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userData = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: "Auth failed" });
+    }
+};
