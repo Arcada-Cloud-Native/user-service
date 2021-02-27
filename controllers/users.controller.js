55768ba43d12cf7721a57be73bbf35e749e620f6
nordluma <nordluma@arcada.fi>
2021-02-27 13:41:41 +0200

    moved signup to controller and getUserById

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const User = require("../models/users.model");
 
@@ -21,21 +22,93 @@ exports.getAllUsers = async (req, res, next) => {
 // @access  Public
 exports.getUserById = async (req, res, next) => {
     try {
-    } catch (err) {}
+        const { id } = req.params;
+        const user = await User.findById(id);
+
+        res.status(200).json(user);
+    } catch (err) {
+        const error = new Error(err);
+        error.status = err.status || 500;
+        next(error);
+    }
 };
 
 // @desc    Update user
 // @route   PATCH /api/users/:id
 // @access  Protected
-exports.updateUser = async (req, res, next) => {};
+exports.updateUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const owner = await User.findById(id);
+
+        isOwner(owner._id);
+
+        const user = await User.update(
+            { _id: req.params.id },
+            { $set: req.body }
+        );
+        return res.status(200).json({ message: "User updated", user });
+    } catch (err) {
+        const error = new Error(err);
+        error.status = err.status || 500;
+        next(error);
+    }
+};
 
 // @desc    Delete user
 // @route   DELETE /api/users/:id
 // @access  Protected
+exports.deleteUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const owner = await User.findById(id);
+
+        isOwner(owner._id);
+
+        const user = await User.remove({ _id: id });
+        return res.status(200).json({ message: "User deleted" });
+    } catch (err) {
+        const error = new Error(err);
+        error.status = err.status || 500;
+        next(error);
+    }
+};
 
 // @desc    Register user
 // @route   POST /api/users/signup
 // @access  Public
+exports.userSignup = async (req, res, next) => {
+    try {
+        const { name, address, email, password } = req.body;
+        // Check if user exists
+        const emailExists = await User.findOne({ email });
+        if (emailExists)
+            return res.status(400).json({ message: "User already exists" });
+
+        bcrypt.hash(password, 10, async (err, hash) => {
+            if (err) {
+                res.status(500).json({
+                    message: err,
+                });
+            } else {
+                const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    name,
+                    address,
+                    email,
+                    password: hash,
+                });
+
+                const savedUser = await user.save();
+                res.status(201).json({ message: "User registered" });
+            }
+        });
+    } catch (err) {
+        const error = new Error(err);
+        error.status = err.status || 500;
+        next(error);
+    }
+};
 
 // @desc    User Login
 // @route   POST /api/users
--- routes/users.route.js
+++ routes/users.route.js
@@ -10,6 +10,7 @@ const {
     getUserById,
     updateUser,
     deleteUser,
+    userSignup,
     userLogout,
 } = require("../controllers/users.controller");
 
@@ -22,35 +23,7 @@ route.patch("/:id", checkAuth, updateUser);
 route.delete("/:id", checkAuth, deleteUser);
 route.get("/logout", checkAuth, userLogout);
 
-route.post("/signup", (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            res.status(500).json({
-                message: err,
-            });
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash,
-            });
-
-            user.save()
-                .then((result) => {
-                    res.status(201).json({
-                        message: "User registered!",
-                    });
-                })
-                .catch((error) => {
-                    console.log(error);
-                    const err = new Error(error);
-                    err.status = error.status || 500;
-
-                    next(err);
-                });
-        }
-    });
-});
+route.post("/signup", userSignup);
 
 route.post("/login", (req, res, next) => {
     User.find({ email: req.body.email })
